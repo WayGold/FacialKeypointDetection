304caad94627908e2fdeadd23dd191027781ea51
Wei Zeng <wzeng@ucsd.edu>
2022-12-13 16:26:24 -0700

    docstring update

--- visualizer.py
+++ visualizer.py
@@ -3,12 +3,32 @@ import matplotlib.pyplot as plt
 
 
 def vis_img_kpts(img, kpts):
+    """
+    Draw image and kpts to plot.
+
+    Args:
+        img:            Image data to draw
+        kpts:           Keypoint data to draw
+
+    Returns:            None
+
+    """
     plt.imshow(img.reshape(96, 96), cmap='gray')
     # Kpts is formatted [left_eye_center_x, left_eye_center_y, ...]
     plt.scatter(kpts[::2], kpts[1::2], marker='x', s=10)
 
 
 def rand_vis_dataset(i_dataset, num_vis):
+    """
+    Given the input dataset, randomly visualize num_vis amount of samples.
+
+    Args:
+        i_dataset (FacialKptsDataSet):      Input dataset to draw sample from
+        num_vis (int):                      Num of samples to visualize
+
+    Returns:                                Plots of visualizations
+
+    """
     fig = plt.figure(figsize=(10, 20))
     plt.tight_layout()
 
