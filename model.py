abf9d3643dfb755e04b8f3d08cee8f6e5d83ee8b
Filemon Mateus <filemon.mateus@gmail.com>
2022-12-14 15:02:29 -0700

    merging subclassed resnet models

--- model.py
+++ model.py
@@ -1,27 +1,47 @@
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
+from resnet_utils import ResidualBlock, ResNetStem, ResNetStage, ResidualBottleneckBlock
 
 
-class FullyConnectNet:
-    def __init__(self, lr=1e-2):
+class FullyConnectedNet(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.model = nn.Sequential(
             nn.Linear(96 * 96, 500),
             nn.ReLU(),
             nn.Linear(500, 30)
         )
-        self.optim = optim.Adam(self.model.parameters(), lr=lr)
-
-
-class ResNet(nn.Module):
-    '''
-    TODO: structurally adapt from homework 3
-    '''
-    def __init__(self, lr=1e-2):
-        super(ResNet, self).__init__()
-        self.model = nn.Sequential()
-        self.optim = optim.Adam(self.model.parameters(), lr=lr)
 
     def forward(self, x):
         x = self.model(x)
         return x
+
+
+class ResNet(nn.Module):
+    def __init__(self, stage_args, in_channels=1, block=ResidualBlock, num_classes=30):
+        super().__init__()
+        layers = [ResNetStem(), *[ResNetStage(*stage_arg, block) for stage_arg in stage_args]]
+        self.cnn = nn.Sequential(*layers)
+        self.fcc = nn.Linear(stage_args[-1][1], num_classes)
+
+    def forward(self, x):
+        x = self.cnn(x)
+        H, W = x.shape[2], x.shape[3]
+        x = F.avg_pool2d(x, kernel_size=(H,W))
+        x = x.flatten(1,-1)
+        x = self.fcc(x)
+        return x
+
+def resnet32():
+    stage_args = [(8, 8, 5, False), (8, 16, 5, True), (16, 32, 5, True)]
+    return ResNet(stage_args, block=ResidualBlock)
+
+def resnet47():
+    stage_args = [(32, 32, 5, False), (32, 64, 5, True), (64, 128, 5, True)]
+    return ResNet(stage_args, block=ResidualBottleneckBlock)
+
+if __name__ == '__main__':
+    fully_cc_model = FullyConnectedNet()
+    resnet32_model = resnet32()
+    resnet47_model = resnet47()
+    print(fully_cc_model)
---
+++ resnet_utils.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+
+class PlainBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=[2 if downsample else 1], padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super().__init__()
+        self.block = PlainBlock(in_channels, out_channels, downsample)
+        if not downsample:
+            self.shortcut = nn.Identity() if in_channels == out_channels else nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        else:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2, padding=0)
+
+    def forward(self, x):
+        x = self.block(x) + self.shortcut(x)
+        return x
+
+
+class ResNetStage(nn.Module):
+    def __init__(self, in_channels, out_channels, num_blocks, downsample=True, block=ResidualBlock):
+        super().__init__()
+        blocks = [block(in_channels, out_channels, downsample)]
+        for _ in range(num_blocks - 1):
+            blocks.append(block(out_channels, out_channels))
+        self.model = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class ResNetStem(nn.Module):
+    def __init__(self, in_channels=3, out_channels=8):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class ResidualBottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels // 4, kernel_size=1, stride=[2 if downsample else 1], padding=0),
+            nn.BatchNorm2d(out_channels // 4),
+            nn.ReLU(),
+            nn.Conv2d(out_channels // 4, out_channels // 4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels // 4),
+            nn.ReLU(),
+            nn.Conv2d(out_channels // 4, out_channels, kernel_size=1, stride=1, padding=0)
+        )
+        if not downsample:
+            self.shortcut = nn.Identity() if in_channels == out_channels else nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        else:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2, padding=0)
+
+
+    def forward(self, x):
+        x = self.block(x) + self.shortcut(x)
+        return x
